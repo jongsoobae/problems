c1ba630b5481eff2173527092aaf97da3f6877d2
zephyrrr <ocpek@nate.com>
2017-04-07 00:41:20 +0900

    fix multplying, lru-cache

--- codewars/lru-cache/p.js
+++ codewars/lru-cache/p.js
@@ -1,175 +1,137 @@
-class Node {
-	constructor(v) {
-		this.v = v;
-		this.next = null;
-		this.prev = null;	
-	}
-	
+class LRUCache{
+    constructor(n, o) {
+      Object.defineProperty(this, '_', {
+        enumerable: false,
+        configurable: true,
+        value: {}
+      });
+
+      Object.defineProperty(this, 'size', {
+        enumerable: false,
+        configurable: false,
+        writable: true,
+        value: 0
+      });
+
+      Object.defineProperty(this, 'lrucounter', {
+        enumerable: false,
+        configurable: true,
+        value: []
+      });
+
+      Object.defineProperty(this, 'capacity', {
+        enumerable: false,
+        configurable: false,
+        get: function() {return this._.capacity;},
+        set: function(n) {
+          this._.capacity = n;
+          while(this.size > 1) {
+            this._delete_key(this._counter_shift());
+            this.size--;
+          }
+        }
+      });
+
+      Object.defineProperty(this, 'delete', {
+        enumerable: false,
+        configurable: false,
+        get: function() {
+          return function(k) {
+            if(k in this._) this.size--;
+            this._counter_remove(k);
+            return this._delete_key(k);
+          }
+        }
+      });
+
+
+
+      this.capacity = n;
+
+      for(let k in o) {
+        if(o.hasOwnProperty(k))
+          this.cache(k, o[k]);
+      }
+    }
+
+    cache(k, v) {
+      this._define_prop(k);
+      this[k] = v;
+
+      if(this.size == this.capacity) {
+        this._timedout();
+      }
+      this.size = this.lrucounter.length;
+      return this;
+    }
+
+    _delete_key (k) {
+      try {
+        delete this._[k];
+        return delete this[k];
+      }
+      catch(e) {
+        return false;
+      }
+
+    }
+
+    _timedout() {
+      this._delete_key(this._counter_shift());
+    }
+
+    _counter_push(k) {
+      this.lrucounter.push(k);
+    }
+    _counter_remove(k) {
+      let idx = this.lrucounter.indexOf(k);
+      if(idx >= 0) this.lrucounter.splice(idx,1);
+    }
+    _counter_exists(k) {
+      return this.lrucounter.indexOf(k) >= 0;
+    }
+    _counter_pop() {
+      return this.lrucounter.pop();
+    }
+    _counter_shift() {
+      return this.lrucounter.shift();
+    }
+
+    _define_prop(k) {
+      Object.defineProperty(this, k, {
+        enumerable: true,
+        configurable: true,
+        get: function() {return this._[k];},
+        set: function(v) {
+          this._[k] = v;
+          this._counter_remove(k);
+          this._counter_push(k);
+        }
+      });
+    }
 }
 
-class Stack {
-	constructor() {
-		this.head = null;
-		this.tail = null;	
-	}
-	
-	unshift(v) {
-		if(this.head === null) {
-			this.head = new Node(v);
-			this.tail = this.head;
-		}
-		else {
-			var temp = new Node(v);
-			temp.next = this.head;
-			this.head.prev = temp;
-			this.head = temp;
-		}
-	}
+let store = new LRUCache(3, {'a': 1});
 
-	push(v) {
-		if (this.tail === null) {
-            this.tail = new Node(v);
-            this.head = this.tail;
-        } else {
-            var temp = new Node(v);
-            temp.prev = this.tail;
-            this.tail.next = temp;
-            this.tail = temp;
-        }
-	}
-
-	pop() {
-		var ret = null;
-
-        if (this.tail !== null) {
-            ret = this.tail.data;
-
-            if (this.tail === this.head) {
-                this.head = null;
-                this.tail = null;
-            } else {
-                this.tail = this.tail.prev;
-                this.tail.next = null;
-            }
-        }
-
-        return ret;
-	}
-
-	pop_value(v) {
-		var curr = this.find(v);
-		if(curr && curr.next != null) {
-			if(curr.prev) curr.prev.next = curr.next;
-			curr.next.prev = curr.prev;
-			curr.next = null;
-			curr.prev = null;
-		}
-	}
-
-	shift() {
-		var ret = null;
-
-        if (this.head !== null) {
-            ret = this.head.v;
-
-            if (this.tail === this.head) {
-                this.head = null;
-                this.tail = null;
-            } else {
-                this.head = this.head.next;
-                if(this.head)this.head.prev = null;
-            }
-        }
-        return ret;
-	}
-
-	find(v) {
-		var curr = this.head;
-		while (curr && curr.v != v) {
-			curr = curr.next;
-		}
-
-		return curr;
-	}
-
-	each(f) {
-		var curr = this.head;
-        while (curr !== null) {
-            f(curr);
-            curr = curr.next;
-        }
-	}
-
-	printList() {
-		console.log('\n======== stack start ========');
-		this.each(function (item) {
-            console.log(item.v);
-        });
-        console.log('======== stack end ========\n');
-	}
-}
-
-class LRUCache {
-	constructor(capacity, opts) {
-		this.stack = new Stack();
-		this.capacity = capacity;
-		this.size = 0;
-		this.properties = {};
-		for(var k in opts) {
-			this.size++;
-			this.stack.push(k);
-			this.objectDefine(k, opts[k]);
-		}
-		this.stack.printList();
-	}
-
-	cache(k, v) {
-		if(k in this) {
-			this[k] = v;
-			this.stack.pop_value(k);
-			this.stack.push(k);
-			return this;
-		}
-
-		if(this.size == this.capacity) {
-			var x = this.stack.shift();
-			console.log('timedout', x);
-			delete this.properties[x];
-			this.size--;
-		}
-
-		this.size++;
-		this.stack.push(k);
-		this.objectDefine(k, v);
-		return this;
-	}
-
-	objectDefine(k, v) {
-		this.properties[k] = v;
-		Object.defineProperty(this, k, {
-			get: function() {
-				this.stack.printList();
-				this.stack.pop_value(k);
-				this.stack.push(k);
-				return this.properties[k];
-			},
-			set: function(_v) {
-				this.properties[k] = _v;
-				this.stack.pop_value(k);
-				this.stack.push(k);
-			}
-		});
-	}
-};
-
-var store = new LRUCache(3, {a: 1});
-
-console.log('store.size', store.size);
-console.log('store.capacity', store.capacity);
-console.log('store.a', store.a);
-console.log('store.b', store.b);
-console.log("store.cache('b', 2)['b']", store.cache('b', 2)['b']);
+console.log('size',store.size); // should be 1
+console.log('capacity', store.capacity); // should be 3
+console.log('a', store.a); // should be 1;
+console.log('b', store.cache('b', 2).b); // should be 2
 store.a = 5;
-console.log('store.a', store.a);
-console.log("store.cache('c', 3).cache('d', 4).b", store.cache('c', 3).cache('d', 4).b);
+console.log(store.a); // should be 5
+console.log(store.cache('c', 3).cache('d', 4).b);
 
+store.delete('d');
+console.log(store.d);
+console.log(store.size);
+console.log(store.c);
+store.cache('c', 4);
+console.log(store.c);
+console.log(store.lrucounter);
+console.log('keke', delete store['delete']);
+console.log(store.delete('delete'));
+console.log(store.delete('e'));
+
+store.capacity = 1; // should resize the store to have just one element
+console.log(Object.keys(store));
+console.log(store);
---
+++ codewars/multiplying-numbers-as-strings_3kyu/p.js
@@ -0,0 +1,27 @@
+function multiply(a, b) {
+  "use strict";
+  if(!a) a = '0';
+  if(!b) b = '0';
+
+  let i, j, k;
+
+  let mul = new Array(a.length+b.length+1).fill(0);
+  for( i = 0 ; i < a.length; i++ ) {
+    for( j = 0 ; j < b.length; j++ ) {
+      k = a.length + b.length-i-j;
+
+      let aa = parseInt(a[a.length-i-1]);
+      let bb = parseInt(b[b.length-j-1]);
+      mul[k] += aa * bb;
+
+      if(mul[k] > 9) {
+        mul[k-1] = mul[k-1] + Math.floor(mul[k]/10);
+        mul[k] = mul[k] % 10;
+      }
+    }
+  }
+  return mul.join('').replace(/^0+/, '') || '0';
+}
+
+console.log(multiply("1020303004875647366210", "2774537626200857473632627613"));
+console.log(multiply("", "2774537626200857473632627613"));
