a5f96150a7c14c1bdbbdf1e37ee151dc5e7ecbaa
jongsoo.bae <jongsoo.bae@cdnetworks.co.kr>
2017-04-07 10:50:25 +0900

    fix multiply

--- codewars/multiplying-numbers-as-strings_3kyu/p.js
+++ codewars/multiplying-numbers-as-strings_3kyu/p.js
@@ -3,9 +3,22 @@ function multiply(a, b) {
   if(!a) a = '0';
   if(!b) b = '0';
 
-  let i, j, k;
+  let k, mul = new Array(a.length+b.length+1).fill(0);
 
-  let mul = new Array(a.length+b.length+1).fill(0);
+  a.split('').reverse().forEach(function(va, ia) {
+    b.split('').reverse().forEach(function(vb, ib) {
+      k = ia + ib;
+      mul[k] += parseInt(va) * parseInt(vb);
+
+      if(mul[k] > 9) {
+        mul[k+1] = mul[k+1] + Math.floor(mul[k]/10);
+        mul[k] = mul[k] % 10;
+      }
+    });
+  });
+
+  return mul.reverse().join('').replace(/^0+/, '') || '0';
+  /*
   for( i = 0 ; i < a.length; i++ ) {
     for( j = 0 ; j < b.length; j++ ) {
       k = a.length + b.length-i-j;
@@ -21,6 +34,7 @@ function multiply(a, b) {
     }
   }
   return mul.join('').replace(/^0+/, '') || '0';
+  */
 }
 
 console.log(multiply("1020303004875647366210", "2774537626200857473632627613"));
